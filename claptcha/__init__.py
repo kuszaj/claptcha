6b2ed15d22a0eb1e540e89e709ebb65734c7f880
Piotr Kuszaj <peterkuszaj@gmail.com>
2017-01-30 19:32:05 +0100

    Initial commit

---
+++ claptcha/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .claptcha import Claptcha
---
+++ claptcha/claptcha.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import random
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+
+class Claptcha(object):
+    def __init__(self):
+        pass
